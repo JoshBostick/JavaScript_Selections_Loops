72bb8b62d27a1e9116b4363540cb8cc387651bd5
Josh Bostick <jbostick99@gmail.com>
2024-02-05 15:20:06 -0600

    finished

--- app.js
+++ app.js
@@ -1,7 +1,94 @@
+
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
+
 console.log("EXERCISE 1:\n==========\n");
+
+for (let i = 1; i < 100; i+= 2) {
+    console.log(i);
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 == 0 & i % 5 ==0) {
+        console.log(i + " FIZZBUZZ");
+    } else if (i % 3 == 0) {
+        console.log(i + " FIZZ");
+    } else if (i % 5 == 0) {
+        console.log (i + " BUZZ");
+    }
+}
+
+
+//Exercise 3
+console.log("EXERCISE 3:\n==========\n");
+
+let i = 1;
+
+do {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+    i++
+} while (i < 100);
+
+//--------
+
+let x = 1;
+
+do {
+    if (x % 3 == 0 & x % 5 == 0) {
+        console.log(x + " FIZZBUZZ");
+    } else if (x % 3 ==0) {
+        console.log(x + " FIZZ");
+    } else if (x % 5 == 0) {
+        console.log(x + " BUZZ");
+    }
+    x++
+    } while (x <= 100);
+     
+
+
+// Exercise 4
+console.log("EXERCISE 4:\n==========\n");
+
+let value = Math.round((Math.random() * 500));
+
+let n = Math.round(Math.random() * (500 - 100) + 100);
+
+for (i = 0; i <= n; i++) {
+    if (i == value) {
+        console.log("Range: 1 to "+ n + ". Found " + value);
+        break;
+    }   
+    if (i == n) {
+    console.log("Range: 1 to "+ n + ". Did not find " + value);
+    }
+} 
+
+// Exercise 5
+console.log("EXERCISE 5:\n==========\n");
+
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1);
+let end = Math.round(Math.random() * (1000 - 1) + 1);
+let start = Math.round(Math.random() * (10 - 1) + 1);
+
+for (let i = start; i <= end; i++) {
+    let output = "";
+
+    if ( i % fizzDivisor == 0) {
+        output += "FIZZ";
+    } 
+    if (i % buzzDivisor == 0) {
+        output += "BUZZ";
+     }
+    
+    console.log(i, output);
+ 
+}
+
+console.log(fizzDivisor, buzzDivisor);
